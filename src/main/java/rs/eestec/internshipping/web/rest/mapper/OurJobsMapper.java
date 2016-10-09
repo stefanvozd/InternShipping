6e95fbdff8569385dcdcdee147e77f4216c6b914
stefanvozd <91stefan@gmail.com>
2016-10-09 13:22:02 +0200

    resumesJob phase 4

---
+++ src/main/java/rs/eestec/internshipping/web/rest/mapper/OurJobsMapper.java
@@ -0,0 +1,37 @@
+package rs.eestec.internshipping.web.rest.mapper;
+
+import rs.eestec.internshipping.domain.*;
+import rs.eestec.internshipping.web.rest.dto.JobDTO;
+
+import org.mapstruct.*;
+import java.util.List;
+
+/**
+ * Mapper for the entity Job and its DTO JobDTO.
+ */
+@Mapper(componentModel = "spring", uses = {})
+public interface JobMapper {
+
+    @Mapping(source = "company.id", target = "companyId")
+    @Mapping(source = "company.name", target = "companyName")
+    @Mapping(source = "company.logo", target = "companyLogo")
+    @Mapping(source = "company.logoContentType", target = "companyLogoContentType")
+    JobDTO jobToJobDTO(Job job);
+
+    List<JobDTO> jobsToJobDTOs(List<Job> jobs);
+
+    @Mapping(target = "applications", ignore = true)
+    @Mapping(source = "companyId", target = "company")
+    Job jobDTOToJob(JobDTO jobDTO);
+
+    List<Job> jobDTOsToJobs(List<JobDTO> jobDTOs);
+
+    default Company companyFromId(Long id) {
+        if (id == null) {
+            return null;
+        }
+        Company company = new Company();
+        company.setId(id);
+        return company;
+    }
+}
